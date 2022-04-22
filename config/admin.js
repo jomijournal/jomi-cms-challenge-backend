d20a99d4f63cdb46d2ab282f455cd699e4412e73
ViktoriiaVo <viktoriiavo@softwareplanet.uk.com>
2022-04-22 19:19:16 +0300

    Add random value of "ADMIN_JWT_SECRET"

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'dfee1c43fb8d2e506ad627'),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
